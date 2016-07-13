4a8e48351f75aa607610efef599e2e5e747d9f51
leo277 <yongkai8@hotmail.com>
2016-07-13 14:59:27 +0800

    wired up rails with angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,14 +6,21 @@ angular.module('flapperNews', ['ui.router', 'templates'])
 
   $stateProvider
   .state('home', {
+   resolve: {
+    postPromise: ['posts', function(posts){
+      return posts.getAll();
+    }]
+   },
    url: '/home',
    templateUrl: 'home/_home.html',
    controller: 'MainCtrl'
+
   })
   .state('posts', {
    url: '/posts/{id}',
    templateUrl: 'posts/_posts.html',
    controller: 'PostsCtrl'
+
   });
   $urlRouterProvider.otherwise('home');
  }]);

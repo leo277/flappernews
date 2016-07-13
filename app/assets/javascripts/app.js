d44cef3b8924573de12fafb4c4d3930226fcfd67
leo277 <yongkai8@hotmail.com>
2016-07-13 11:24:38 +0800

    move views to assetpipeline

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', ['ui.router'])
+angular.module('flapperNews', ['ui.router', 'templates'])
 .config([
  '$stateProvider',
  '$urlRouterProvider',
@@ -7,72 +7,15 @@ var app = angular.module('flapperNews', ['ui.router'])
   $stateProvider
   .state('home', {
    url: '/home',
-   templateUrl: '/home.html',
+   templateUrl: 'home/_home.html',
    controller: 'MainCtrl'
   })
   .state('posts', {
    url: '/posts/{id}',
-   templateUrl: '/posts.html',
+   templateUrl: 'posts/_posts.html',
    controller: 'PostsCtrl'
   });
   $urlRouterProvider.otherwise('home');
  }]);
 
-///////////////Factory Methods////////////////////////
-app.factory('posts', [function(){
- var o = {
-  posts: []
- };
- return o;
-}]);
 
-///////Main Ctrl////////////
-app.controller('MainCtrl', [
- '$scope',
- 'posts',
- function($scope, posts){
-  $scope.posts = posts.posts;
-  $scope.test = 'Hello world!';
-  
-  $scope.addPost = function(){
-   if(!$scope.title || $scope.title === '') { return; }
-   $scope.posts.push({
-    title: $scope.title,
-    link: $scope.link,
-    upvotes: 0,
-    comments: [
-    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-    ]
-   });
-
-   $scope.title = '';
-   $scope.link = '';
-  };
-
-  $scope.incrementUpvotes = function(post) {
-   post.upvotes += 1;
-  };
-
- }]);
-
-///////////Post Controller///////////////
-app.controller('PostsCtrl', [
- '$scope',
- '$stateParams',
- 'posts',
- function($scope, $stateParams, posts){
-
-  $scope.post = posts.posts[$stateParams.id];
-
-  $scope.addComment = function(){
-   if($scope.body === '') { return; }
-   $scope.post.comments.push({
-    body: $scope.body,
-    author: 'user',
-    upvotes: 0
-   });
-   $scope.body = '';
-  };
-
- }]);

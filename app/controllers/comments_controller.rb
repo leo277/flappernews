222ed7a336da1b417926907a7dd2b9e7ebb0b10c
leo277 <yongkai8@hotmail.com>
2016-07-13 17:14:53 +0800

    completed learning

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
 before_filter :authenticate_user!, only: [:create, :upvote]
 
-def create 
+def create
  post = Post.find(params[:post_id])
- comment = post.comments.create(comment_params)
+ comment = post.comments.create(comment_params.merge(user_id: current_user.id))
  respond_with post, comment
-end 
+end
 
 def upvote
  post = Post.find(params[:post_id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ def index
 end
 
 def create
- respond_with Post.create(post_params)
+ respond_with Post.create(post_params.merge(user_id: current_user.id))
 end
 
 def show
